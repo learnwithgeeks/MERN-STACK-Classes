b652105d46058a6eb2e2f7e2279f4252d1309b77
viveksharmaui <sharma_vivek62@yahoo.com>
2017-06-28 17:32:50 +0500

    comment

--- Assignment 07/js/comment.js
+++ Assignment 07/js/comment.js
@@ -1,8 +1,10 @@
 var database = firebase.database();
 var x = location.hash;
 var z = x.substring(1);
+var user = localStorage.getItem("username");
 var c = document.getElementById('jumb');
 c.innerHTML=z;
+document.getElementById('user').innerHTML ="<b>User : </b>"+ user;
 var container;
 var b;
 var comment ;
@@ -25,7 +27,8 @@ function addComment()
 function save()
 {
   b=database.ref('comment/'+z).push(
-      comment
+     {comment,
+        by: user}
   );
 }
 function load()
@@ -33,7 +36,7 @@ function load()
   var container = document.getElementById('containerdo');
   database.ref('comment/'+z).once('value').then(function(snapshot) {
     for (var i in snapshot.val()) {
-    container.innerHTML += "<div id="+i+"><img src='./img/arrow.png' width='23px' id='img'><h4 id='topi'>"+snapshot.val()[i]+"</h4></div>";
+        container.innerHTML += "<div id="+i+"><img src='./img/arrow.png' width='23px' id='img'><h4 id='topi'>"+snapshot.val()[i].comment+"</h4> <h6> by "+snapshot.val()[i].by+"</h6></div>";
 }});
     container.innerHTML='';
 }
