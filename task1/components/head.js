0f28e0be8d2a4e3a96f39cf7f588dd347b6b9900
viveksharmaui <sharma_vivek62@yahoo.com>
2017-07-23 12:19:19 +0500

    Task

---
+++ task1/components/head.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default class extends React.Component{
+    render(){
+        return (
+            <Head>
+                <link rel='stylesheet' href='../static/style.css' />
+            </Head>
+        );
+    }
+}
