ae444a090ebdb04e33f7247147211c2e1c965df5
viveksharmaui <sharma_vivek62@yahoo.com>
2017-08-27 13:19:27 +0500

    react task css bootstrap

---
+++ React Task (BOOTSTRAP)/pages/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Head from '../components/head';
+export default class extends React.Component
+{
+  render()
+    {
+    return(
+      <main>
+      <Head />
+      <div className="row" id="main">
+          <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2" id="element">
+            <h3>20</h3>
+            <p>New followers added this month</p>
+          </div>
+          <div className="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-1 col-sm-2 col-xs-offset-1 col-xs-2" id="elementa">
+            <h3>$ 1250</h3>
+            <p>Average Monthly Income</p>
+          </div>
+          <div className="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-1 col-sm-2 col-xs-offset-1 col-xs-2" id="elementa">
+            <h3>$ 13865</h3>
+            <p>Yearly Income Goal</p>
+          </div>
+          <div className="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-2 col-sm-offset-1 col-sm-2 col-xs-offset-1 col-xs-2" id="elementc">
+            <h2>18 deg</h2>
+            <p>Paris</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-offset-9 col-lg-2 col-md-offset-9 col-md-2 col-sm-offset-9 col-sm-2 col-xs-offset-9 col-xs-2" id="aside">
+          <div id="elementd">
+            <p>New Visitors</p>
+            <h3>1.5K</h3>
+            <div> </div>
+          </div>
+          <div id="elemente">
+            <p>Bounce Rate</p>
+            <h3>50%</h3>
+            <div></div>
+          </div>
+          <div id="elementf">
+            <p>Searches</p>
+            <h3>28%</h3>
+            <div></div>
+          </div>
+          <div id="elementg">
+            <p>Traffic</p>
+            <h3>104.5kb</h3>
+            <div></div>
+          </div>
+        </div>
+        <div className="row" id="position">
+          <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9" id="sectiona">
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="commentmain">
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="comment">
+              <h3>15080</h3>
+              <p>Shot Views</p>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="comment">
+              <h3>12000</h3>
+              <p>Likes</p>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="comment">
+              <h3>5100</h3>
+              <p>Comments</p>
+            </div>
+          </div>
+          </div>
+          <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9"  id="sectionb">
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="commentmain">
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="comment">
+              <h3>15080</h3>
+              <p>Shot Views</p>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="comment">
+              <h3>12000</h3>
+              <p>Likes</p>
+            </div>
+            <div classNameName="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="comment">
+              <h3>5100</h3>
+              <p>Comments</p>
+            </div>
+          </div>
+    </div>
+  </div>
+  </div>
+  </main>
+    );
+  }
+}
